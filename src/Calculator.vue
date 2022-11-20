b9dc6ad828504567406aced923b5e576419b9b29
peakevan <111390458+peakevan@users.noreply.github.com>
2022-11-20 17:29:49 -0500

    Add files via upload
    Add

---
+++ src/Calculator.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="calculator">
+    <header>
+      <h1>Calculator</h1>
+    </header>
+    <section class="calculator" id="calculator">
+      <calculator-content></calculator-content>
+    </section>
+  </div>
+</template>
+
+<script>
+import CalculatorContent from 'components/CalculatorContent.vue'
+export default {
+  name: 'calculator',
+  components: {
+    CalculatorContent
+  }
+ }
+
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css?family=Pacifico');
+
+html{
+  font-size:62.5%;
+}
+
+body{
+  background-color:#f4f1f1;
+}
+
+
+header{
+  font-size: 1.8rem;
+  font-family: 'Pacifico', cursive;
+}
+
+header .author{
+  font-size: 1.5rem;
+}
+
+header .author a{
+  text-decoration: none;
+}
+
+header , .calculator {
+  margin: 2.5rem auto;
+  text-align: center;
+}
+
+.calculator{
+  border: 0.8rem solid #404040;
+  width: 34rem;
+  height: 40rem;
+  background-color: #404040;
+  font-size: 1.6rem;
+  border-radius: 1rem;: 
+  box-shadow: 1rem 1rem 0.5rem #ccc;
+}
+</style>
